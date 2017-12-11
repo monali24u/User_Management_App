8c8218c6c72c405281350d9e71e206ecafe06fe4
sunilgaikwad <sunilgaikwad@gmail.com>
2017-12-11 08:00:07 -0700

    Through actions created interface so that view could send user actions which can be diverted to the store using dispatcher.

--- src/js/actions/actions.flux.js
+++ src/js/actions/actions.flux.js
@@ -1,8 +1,35 @@
-// var um_dispatcher = require('../dispatcher/dispatcher.flux');
-// var um_constants = require('../constants/constants.flux');
-//
-// var um_actions = {
-//
-// }
-//
-// module.exports = um_actions;
+import dispatcher from "../dispatcher/dispatcher.flux"
+import constants from "../constants/constants.flux"
+
+export function createUser(firstname, lastname, address) {
+  dispatcher.dispatch({
+    type: constants.ADD_USER,
+    firstname,
+    lastname,
+    address,
+  });
+}
+
+export function deleteUser(user) {
+  dispatcher.dispatch({
+    type: constants.DELETE_USER,
+    user,
+  });
+}
+
+export function editUser(user) {
+  dispatcher.dispatch({
+    type: constants.EDIT_USER,
+    user,
+  });
+}
+
+export function updateUser(id, firstname, lastname, address) {
+  dispatcher.dispatch({
+    type: constants.UPDATE_USER,
+    id,
+    firstname,
+    lastname,
+    address,
+  });
+}

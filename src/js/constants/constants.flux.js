66ff2ffa891cb4ddc2d8d5286cdf90cc912c34ab
sunilgaikwad <sunilgaikwad@gmail.com>
2017-12-11 08:02:31 -0700

    standardized action types using constants will certainly use this in store.

--- src/js/constants/constants.flux.js
+++ src/js/constants/constants.flux.js
@@ -1,3 +1,6 @@
-// module.exports = {
-//   ADD_USER: 'ADD_USER',
-// };
+ module.exports = {
+   ADD_USER: 'ADD_USER',
+   DELETE_USER: 'DELETE_USER',
+   EDIT_USER: 'EDIT_USER',
+   UPDATE_USER: 'UPDATE_USER',
+ };

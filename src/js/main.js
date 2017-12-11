e37d9d8cd985d2e5b3b9c8cd2509d2a8f50dc640
sunilgaikwad <sunilgaikwad@gmail.com>
2017-12-11 00:38:21 -0700

    Added entry to the main app! This is the place where we create the main react DOM

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/components.react';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/components.react";
 
 ReactDOM.render(<App />, document.getElementById('main'));
